a1005c81ef335854f9c374d89c8e480a477c0e63
Chana96 <ahasithachandana@gmail.com>
2021-05-13 21:56:53 +0530

    admin attributes added, git ignore setup

--- src/main/java/com/programming/techie/OnlineLibrary/models/Admin.java
+++ src/main/java/com/programming/techie/OnlineLibrary/models/Admin.java
@@ -7,37 +7,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table
-
 public class Admin {
 	@Id
-	@Column(name = "a_id")
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private int a_id;
-	@Column(name = "af_name")
-	private String af_name;
-	@Column(name = "al_name")
-	private String al_name;
+	private int id;
+	private String name;
+	private String email;
+	private String phone;
 	
-	public int getA_id() {
-		return a_id;
-	}
-	public void setA_id(int a_id) {
-		this.a_id = a_id;
-	}
-	public String getAf_name() {
-		return af_name;
-	}
-	public void setAf_name(String af_name) {
-		this.af_name = af_name;
-	}
-	public String getAl_name() {
-		return al_name;
-	}
-	public void setAl_name(String al_name) {
-		this.al_name = al_name;
-	}
-	
-	
+
 }
